c3ee9ff55a7c7953d1a69b7d27b3c38cdd597c71
rusagaib <r.dewangga.ra@gmail.com>
2021-09-14 20:33:40 +0700

    adding requiremets.txt & some of code

--- ytp.sh
+++ ytp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 terminal=`tty`
-exec < ~/erhost/youtubevideolinks.txt
-
+dir=$(pwd)
+ENV_DIR=${dir}/env/bin/activate
+echo ${ENV_DIR}
+source ${ENV_DIR}
+exec < ${dir}/youtubevideolinks.txt
 while read -r line
 do
     cd ~/Music
     youtube-dl -f 18 -x --extract-audio --audio-format mp3 --audio-quality 0 "$line"
 done
+deactivate
+cd ${dir}
 exec < "$terminal"
